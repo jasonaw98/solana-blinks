26bb8cf3c8044f4772709dc8f87677c128c3e2a7
jason.aw <jason_aw1998@hotmail.com>
2024-07-17 17:33:28 +0800

    add new page

--- app/actions.json/route.ts
+++ app/actions.json/route.ts
@@ -3,17 +3,10 @@ import { ACTIONS_CORS_HEADERS, ActionsJson } from "@solana/actions";
 export const GET = async () => {
   const payload: ActionsJson = {
     rules: [
-      // map all root level routes to an action
       {
-        pathPattern: "/now*",
-        apiPath: "/api/actions/donate*",
+        pathPattern: "/blink",
+        apiPath: "/api/actions/donate",
       },
-      // idempotent rule as the fallback
-      /*
-      Idempotent rules allow blink clients to more easily determine if a given path supports 
-      Action API requests without having to be prefixed with the solana-action: 
-      URI or performing additional response testing.
-      */
       {
         pathPattern: "/api/actions/**",
         apiPath: "/api/actions/**",
@@ -26,6 +19,4 @@ export const GET = async () => {
   });
 };
 
-// DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
-// THIS WILL ENSURE CORS WORKS FOR BLINKS
 export const OPTIONS = GET;
---
+++ app/blink/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen w-full justify-center items-center flex flex-col'>
+        <h1 className='text-3xl font-bold'>This is a blink</h1>
+        <h1 className='text-3xl font-bold'>Try posting this link on Twitter!</h1>
+    </div>
+  )
+}
+
+export default page
