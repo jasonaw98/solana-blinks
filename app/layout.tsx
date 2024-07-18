a9741d05adddd3781235df83683ab707ddb206d8
jason.aw <jason_aw1998@hotmail.com>
2024-07-18 17:38:58 +0800

    update metadata tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,31 +5,26 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(`https://nick.af`),
-  title: {
-    default: "Jason",
-    template: `%s Hello`,
-  },
-  description:
-    "Hi! I'm Nick, a full stack developer and submariner working on various " +
-    "projects. In my free time I write software, technical articles, and build things.",
+  title: 'Jason Blink',
+  description: 'Your website description',
   openGraph: {
-    siteName: "Jason",
-    type: "website",
+    title: 'Your Website Title',
+    description: 'Your website description',
     images: [
       {
-        url: "/therock.webp",
-        width: 256,
-        height: 256,
-        alt: "therock",
+        url: 'https://github.com/shadcn.png',
+        width: 1200,
+        height: 630,
+        alt: 'Open Graph Image Alt',
       },
     ],
   },
   twitter: {
-    site: `@jasonaw98`,
-    creator: `@jasonaw98`,
-    card: "summary_large_image",
-    images: ["/therock.webp"],
+    card: 'summary',
+    title: 'Blink',
+    description: 'Your website description',
+    images: ['https://github.com/shadcn.png'],
+    creator: '@jasonaw98',
   },
   category: "technology",
 };
@@ -41,12 +36,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="twitter:image" content="<generated>" />
-        <meta name="twitter:image:type" content="<generated>" />
-        <meta name="twitter:image:width" content="<generated>" />
-        <meta name="twitter:image:height" content="<generated>" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
